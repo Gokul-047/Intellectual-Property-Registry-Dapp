49bfcc1b5ddc4544b3f9343c5d15cd6e3a179327
Gokul-047 <gokulworld02@gmail.com>
2025-11-16 15:47:18 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -4,4 +4,4 @@
 export const SEPOLIA_CHAIN_ID = "0xaa36a7";
 
 // Your Sepolia contract address
-export const CONTRACT_ADDRESS = "0x12443acd3b2413a0fc18e99ada433590e18a5a20";
+export const CONTRACT_ADDRESS = "Your Sepolia contract address";
